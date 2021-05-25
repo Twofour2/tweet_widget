1e5cde53e074cc3a94e5ad9bdb69cc32186462bf
chaosay <38771541+chaos-a@users.noreply.github.com>
2021-05-25 22:45:12 +1200

    removed buggy attempt to work around the image upload bug. it now functions properly. Also images are uploaded at the correct resolution.

--- twitter-reddit-new.py
+++ twitter-reddit-new.py
@@ -73,47 +73,20 @@ def Main():
             logging.info(f"### {twSub.Name} (uploadImages: {uploadImages}) ###")
             twSub.loadConfig()
             try:
-                if uploadImages: # this instance of the script ONLY uploads images every so often
-                    logging.info("Running in upload images mode")
+                if datetime.utcnow().timestamp() > nextImageUploadDate: # specify when
                     twSub.uploadImages()
-                else:
-                    if datetime.utcnow().timestamp() > nextImageUploadDate:
-                        uploadProfileImages(allSubreddits)
-                    else: # regular mode, just update the tweets
-                        twSub.updateWidget()
+                    twSub.bugFixImageUpload = True
+                    nextImageUploadDate = (datetime.utcnow() + timedelta(days=1)).timestamp()
+                    logging.info(f"Done uploading images to all subreddits, next upload is scheduled for: {datetime.fromtimestamp(nextImageUploadDate)}")
+                twSub.updateWidget()
+
             except timeout_decorator.TimeoutError as e:
                 twSub.logFailure(f"{twSub.Name}: Timed out ({e})", exception=e)
             except Exception as e:
                 twSub.logFailure(f"{twSub.Name}: Other exception: {e}", exception=e)
         else:
-            if uploadImages:
-                logging.info("Done uploading images for all subreddits. Exiting temp script.")
-                exit(150) # exit the temp script run by subprocess and return back to the main script
-            else:
-                logging.info("Done with widgets, waiting 5 mins")
-                time.sleep(300)
-
-
-def uploadProfileImages(allSubreddits):
-    logging.info("----- Uploading new profile images ------")
-    global nextImageUploadDate
-    # Note: if we try to call twSub.uploadImages() from this "main" instance of this script, PRAW seems(?) to cache the widget upload item.
-    # We need to upload twice to the subreddit as reddit refuses to show images on unless the css is updated on a second upload. This happens in the normal widget UI and has been a bug for a long time.
-    # We need to run another instance of this script as if it is run from the same main script praw will use a cache of the first widget upload. Which causes the images to break and become grey
-    # TLDR: We need to run another copy of this script because new reddits widget system is buggy and broken
-    try:
-        uploadImagesProcess = subprocess.run([sys.executable or 'python3', script_dir + "/twitter-reddit-new.py", "-u"], timeout=500)  # note: incorrect usage can cause this to loop. shell=True needs to be added for windows systems
-        if uploadImagesProcess.returncode == 150:
-            for twSub in allSubreddits: # go through all of the subreddits and perform the bugfix version of the widget upload
-                twSub.bugFixImageUpload = True
-                twSub.updateWidget()
-                logging.info(f"{twSub.Name}: Finished uploading images")
-            nextImageUploadDate = (datetime.utcnow() + timedelta(days=1)).timestamp()
-            logging.info(f"Done uploading images to all subreddits, next upload is scheduled for: {datetime.fromtimestamp(nextImageUploadDate)}")
-        else:
-            logging.error(f"Upload images returned another return code {uploadImagesProcess.returncode}")
-    except subprocess.TimeoutExpired as e:
-        logging.error(f"Upload images process timed out: {e}")
+            logging.info("Done with widgets, waiting 5 mins")
+            time.sleep(300)
 
 def dbConnect(botconfig):
     # DB Connection
